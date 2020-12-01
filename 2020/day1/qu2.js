a2adaab1ee5610cba922c1b50920d2ce6892f1b1
Rupert Barton <bartonrupert@hotmail.com>
2020-12-01 11:18:10 +0000

    Day1

---
+++ 2020/day1/qu2.js
@@ -0,0 +1,15 @@
+const input = require("./input.json");
+
+const fixExpenses = expenses => {
+  for (let i = 0; i < expenses.length - 2; i++) {
+    for (let j = i + 1; j < expenses.length - 1; j++) {
+      for (let k = j + 1; k < expenses.length; k++) {
+        if (expenses[i] + expenses[j] + expenses[k] === 2020) {
+          return expenses[i] * expenses[j] * expenses[k];
+        }
+      }
+    }
+  }
+};
+
+console.log(fixExpenses(input));
