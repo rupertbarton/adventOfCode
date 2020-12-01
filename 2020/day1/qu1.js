79ab129cfb9009e330146b5ad1833e0a68c5250b
Rupert Barton <bartonrupert@hotmail.com>
2020-12-01 11:18:10 +0000

    Day1

---
+++ 2020/day1/qu1.js
@@ -0,0 +1,18 @@
+const input = require('./input.json')
+
+const fixExpenses = (expenses) => {
+    const differences = {}
+
+    for (let i = 0; i < expenses.length; i++) {
+        const current = expenses[i]
+        if (differences[current]) {
+            return differences[current] * current
+        } else {
+            differences[2020 - current] = current
+        }
+    }
+}
+
+const testArray = [1721,979,366,299,675,1456]
+
+console.log(fixExpenses(input))
